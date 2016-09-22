835bbf7e63c923206e8212424d2c613e78107817
Dmitriy Arkhipov <dmitriy.arkhipov.biz@mail.ru>
2016-09-22 13:18:26 +0700

    Initial Commit

---
+++ VacancyApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VacancyApp
+//
+//  Created by Dmitriy Arkhipov on 22.09.16.
+//  Copyright © 2016 Dmitriy Arkhipov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VacancyApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VacancyApp
+//
+//  Created by Dmitriy Arkhipov on 22.09.16.
+//  Copyright © 2016 Dmitriy Arkhipov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
